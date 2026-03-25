aa63e78967175f71e87027caa1ab3bc81fbc43db
Rodrigo Torres <rtgf@cesar.school>
2026-03-24 21:02:54 -0300

    Create CucumberTest.java

---
+++ bdd/acadtrackbdd/src/test/java/br/com/acadtrack/bdd/CucumberTest.java
@@ -0,0 +1,15 @@
+package br.com.acadtrack.bdd;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "br.com.acadtrack.bdd")
+public class CucumberTest {
+}
